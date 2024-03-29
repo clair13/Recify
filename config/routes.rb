a7c06916212f7a8a573e086c38131ffe7e42d1cb
clair13 <theclairvoyant1313@yahoo.gr>
2024-03-29 19:13:21 +0200

    Add nested routes and render instructions in the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :steps
-  resources :recipes
+  
+  resources :recipes do
+    resources :steps
+  end
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
