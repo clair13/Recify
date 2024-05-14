ce8fbd0f5ddac06f00663d74ac147bb5b6b58745
clair13 <theclairvoyant1313@yahoo.gr>
2024-05-14 18:52:54 +0300

    Add broadcast refreshes for steps

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :steps, dependent: :destroy
+
+  broadcasts_refreshes
   
   has_one_attached :image do |attachable|
     attachable.variant :thumb,  resize_to_limit: [100, 100]
